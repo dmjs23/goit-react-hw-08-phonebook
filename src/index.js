1599f42ce93313c5a25ab56c153e3f80ce95a77a
Kamil Przebieracz <dmjs23@gmail.com>
2022-08-16 18:53:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-          <App/>
+          <App />
         </BrowserRouter>
       </PersistGate>
-   </Provider>
+    </Provider>
   </React.StrictMode>
-)
+  //test comita z kluczem podpisu
+);
